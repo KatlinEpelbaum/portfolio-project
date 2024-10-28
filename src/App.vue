e0af5ab431d6869a36b77639996f84a95a9a636b
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2024-10-28 12:31:48 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <SkillsSection />
     <ServicesSection />
     <ProcessSection />
+    <Timeline />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import Navbar from './components/navbar.vue';
 import SkillsSection from './components/skillsSection.vue';
 import ServicesSection from './components/servicesSection.vue';
 import ProcessSection from './components/processSection.vue';
+import Timeline from './components/Timeline.vue';
 
 export default {
   name: 'App',
@@ -20,6 +22,7 @@ export default {
     SkillsSection,
     ServicesSection,
     ProcessSection,
+    Timeline,
   },
 };
 </script>
--- src/components/processSection.vue
+++ src/components/processSection.vue
@@ -7,19 +7,6 @@
         </h1>
         <p class="font-semibold text-slate-800">Our process ensures that we create a website <br> tailored to your business needs.</p>
       </div>
-      <div class="grid grid-cols-2 gap-4">
-    <div>
-        
-    </div>
-    <div class="p-4 bg-green-500 text-white">Item 2</div>
-    <div class="p-4 bg-red-500 text-white">Item 3</div>
-    <div class="p-4 bg-yellow-500 text-white">Item 4</div>
-    <div class="p-4 bg-purple-500 text-white">Item 5</div>
-    <div class="p-4 bg-purple-500 text-white">Item 5</div>
-    <div class="p-4 bg-purple-500 text-white">Item 5</div>
-    <div class="p-4 bg-purple-500 text-white">Item 5</div>
-</div>
-
     </section>
   </template>
   
