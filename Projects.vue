0408f7c7f24e4bcabac9d95018b10cdc29868735
Kätlin <145542776+KatlinEpelbaum@users.noreply.github.com>
2024-12-12 09:14:30 +0200

    Projects page

---
+++ Projects.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-8xl py-20 px-32 font-bold" style="font-family: 'Pilcrow Rounded', sans-serif;">Projects & <br> Self-analysis</h1>
+        <router-link
+        to="/contact"
+        class="ml-32 px-8 py-4 bg-pink-400 hover:bg-pink-500 font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+        Get In Touch
+        </router-link>
+        <img src="" alt="">
+    </div>
+</template>
